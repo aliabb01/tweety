1070167e90797bfc8814025b0f288c473a7c5ea1
aliabb01 <aliabbasov21112001@gmail.com>
2021-03-19 22:54:27 +0400

    Sidebar-links, publish-tweet-panel, friends-list and tweet blade views

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,15 @@
+<h3 class="font-bold text-xl mb-4">Following</h3>
+
+<ul>
+    @foreach (auth()->user()->follows as $user)
+
+
+        <li class="mb-4">
+            <div class="flex items-center text-sm">
+                <img src="{{ $user->avatar }}" class="rounded-full mr-2" alt="">
+                {{ $user->name }}
+            </div>
+        </li>
+
+    @endforeach
+</ul>
