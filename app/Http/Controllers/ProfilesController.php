9d0ddc5c2366f518f002fac3c92f0bfb1812f350
aliabb01 <aliabbasov21112001@gmail.com>
2021-03-20 17:36:07 +0400

    Initial edit view and ways of checking for edit authorization in the ProfilesController@edit

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,4 +11,23 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', compact('user'));
     }
+
+    public function edit(User $user)
+    {
+        // Two ways of stopping editing profile of others (/edit) through writing url (63)
+
+        // First way of doing it  (63)
+        // if($user->isNot(current_user())){  
+        //     abort(404);
+        // }
+
+        // Second way of doing it  (63)
+        // abort_if($user->isNot(current_user()), 404);
+
+        // Another way of doing the same thing however, this time it returns an error of 403
+        //$this->authorize('edit', $user);  (63)
+        //Finally this was done in routes (63)
+
+        return view('profiles.edit', compact('user'));
+    }
 }
