caabd9c1f6e4ecb850968a00bf4e7f37610ff5cc
aliabb01 <aliabbasov21112001@gmail.com>
2021-03-21 15:44:48 +0400

    Update function added to ProfilesController

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Validation\Rule; // (64) This is need in order to make Rule::unique() work
 
 class ProfilesController extends Controller
 {
@@ -29,5 +30,23 @@ class ProfilesController extends Controller
         //Finally this was done in routes (63)
 
         return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        // dd(request('avatar'));  (64) this returns UploadedFile instance
+        $attributes = request()->validate([
+            'username' => ['string', 'required', 'max:255', 'alpha_dash', Rule::unique('users')->ignore($user)],  // (64) Rule::unique basically selects unique username from users table and when updating users own profile it ignores that editing user to prevent update blocking 
+            'name' => ['string', 'required', 'max:255'],
+            'avatar' => ['required', 'file'],
+            'email' => ['string', 'required', 'email', 'max:255', Rule::unique('users')->ignore($user)],
+            'password' => ['string', 'required', 'min:8', 'max:255', 'confirmed'], // (64) 'confirmed' looks for password_confirmation
+        ]);
+
+        $attributes['avatar'] = request('avatar')->store('avatars');  // (64) uploaded file is being saved in storage->app->public->avatars. By default it is storage->app->avatars. You change it by adding FILESYSTEM_DRIVER in env file. In addition command: php artisan storage:link creates a symlink for the uploaded images in public folder under public->storage
+
+        $user->update($attributes);  // (64) Update user information according to the attributes variable above
+
+        return redirect($user->path());  // (64) Redirect back to user profile page
     }
 }
