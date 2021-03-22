bf039887226dbc508ca976c491a273a318272313
aliabb01 <aliabbasov21112001@gmail.com>
2021-03-22 23:14:42 +0400

    Like and dislike forms in a seperate component file

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,8 +1,8 @@
 <div class="flex p-4 {{ $loop->last ? '' : 'border-b border-b-grey-400' }}">
     <div class="mr-2 flex-shrink-0">
 
-        {{-- {{ route('profile', $tweet->user  /*can write "->name" here  (60)*/) }} --}} 
-        <a href="{{ $tweet->user->path() }}">    {{-- return the profile path (62) --}}
+        {{-- {{ route('profile', $tweet->user  /*can write "->name" here  (60)*/) }} --}}
+        <a href="{{ $tweet->user->path() }}"> {{-- return the profile path (62) --}}
             <img src="{{ $tweet->user->avatar }}" class="rounded-full mr-2" width="50" height="50" alt="">
         </a>
     </div>
@@ -16,8 +16,13 @@
             </a>
         </h5>
 
-        <p class="text-sm">
+        <p class="text-sm mb-3">
             {{ $tweet->body }}
         </p>
+
+        {{-- (67) Moving buttons to a seperate fike and making that file understand tweet variable --}}
+        <x-like-buttons :tweet="$tweet"></x-like-buttons>  
+
+
     </div>
 </div>
